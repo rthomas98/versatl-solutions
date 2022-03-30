95f3c1b988ba0768ef1d9b143e176fc8617b0352
Rob Thomas <robt84@gmail.com>
2022-03-29 23:09:23 -0500

    fixed spelling error

--- page-templates/about-page.php
+++ page-templates/about-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Aboit Page Template
+ * Template Name: About Page Template
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
